b771d9244bd7a31f945ad7d9bd947c7885a207e0
Bienvenido Velez <bvelez@acerola.ece.uprm.edu>
2018-03-20 23:43:45 -0400

    Added UPRM status line"

--- src/behaviours/src/ROSAdapter.cpp
+++ src/behaviours/src/ROSAdapter.cpp
@@ -680,7 +680,7 @@ void joyCmdHandler(const sensor_msgs::Joy::ConstPtr& message) {
 
 void publishStatusTimerEventHandler(const ros::TimerEvent&) {
   std_msgs::String msg;
-  msg.data = "online";
+  msg.data = "UPRM";
   status_publisher.publish(msg);
 }
 
